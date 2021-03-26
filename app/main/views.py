6930ef5a2e8363f477df09802b2c090cf3071cf9
Charity Mutoni <charitytoni0@gmail.com>
2021-03-27 00:50:41 +0200

    made changes

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, url_for, abort
 from . import main
 from ..models import User, Pitch, Comment
 from .forms import PitchForm, CommentForm
-from .. import db, photos
+from .. import db
 from flask_login import login_required, current_user
 from datetime import datetime
 
