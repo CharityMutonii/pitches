f41b6da83b94931f33f5b5fae3101007ede1e1af
Charity Mutoni <charitytoni0@gmail.com>
2021-03-27 01:23:53 +0200

    configure uploadset

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://charity:21193@localhost/pitches'
-     
+    UPLOADED_PHOTOS_DEST ='app/static/photos' 
 
 class ProdConfig(Config):
 
