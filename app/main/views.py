88063fd54806f759549f8cf96528f973f13f3589
Charity Mutoni <charitytoni0@gmail.com>
2021-03-29 10:08:03 +0200

    added an import

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from ..models import User, Pitch, Comment
-from .forms import PitchForm, CommentForm
+from .forms import PitchForm, CommentForm, updateProfile
 from .. import db,photos
 from flask_login import login_required, current_user
 from datetime import datetime
